acbe1cb008d1b51f4caa10c81fbb4185d7d10f34
lim <543302839@qq.com>
2018-08-30 17:29:18 +0800

    导航栏

---
+++ 项目代码/lvjin/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<div class="nav">
+			<div class="center"> 
+				<ul class="list_unstyled ul_inline clearfix color_fff font_18">
+					<li>行业动态管控</li>
+					<li>法律动态管控</li>
+					<li>视频课程</li>
+					<li>音频课程</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods:{
+			
+		},
+		mounted(){
+			
+		}
+	}
+</script>
+<style lang='less' scoped>
+	.nav{
+		background: #00AA88;
+		.center{
+			width:1200px;margin:0 auto;
+			>ul>li{
+				padding: 20px 23px;cursor: pointer;
+			}
+		}
+	}
+</style>
--- 项目代码/lvjin/src/view/shopMall/index.vue
+++ 项目代码/lvjin/src/view/shopMall/index.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
     	<Header></Header>
+    	<NavBar></NavBar>
     	<Footer></Footer>
     </div>
 </template>
 <script>
 import Header from '../../components/Header.vue'
 import Footer from '../../components/Footer.vue'
+import NavBar from '../../components/NavBar.vue'
 export default {
     components : {
         Header,
         Footer,
+        NavBar,
     },
     data() {
         return {
