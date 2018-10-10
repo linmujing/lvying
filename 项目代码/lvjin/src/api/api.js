549ea6873a4659fc73a7114eeafe4c8a47430f29
linmujing <543302839@qq.com>
2018-10-10 17:57:25 +0800

    api

--- 项目代码/lvjin/src/api/api.js
+++ 项目代码/lvjin/src/api/api.js
@@ -45,6 +45,8 @@ export const getProductInfo = params => { return instance.post(`product/info/get
 export const getProductList = params => { return instance.post(`product/info/getProductList`, params ); };
 //获取产品分类列表
 export const getProductCatList = params => { return instance.post(`product/cat/getProductCatList`, params ); };
+//获取橱窗推荐商品
+export const getShowCaseProduct = params => { return instance.post(`product/info/getShowCaseProduct`, params ); };
 
 /** 系统接口 **/
 //短信发送
@@ -52,7 +54,9 @@ export const sendSms = params => { return instance.post(`system/sms/sendSms`, pa
 //获取轮播图列表
 // export const getBannerList = params => { return instance.post(`system/banner/getBannerList`, params ); };
 //获取橱窗列表
-export const getBannerList = params => { return instance.post(`system/showCase/getBannerList`, params ); };
+export const getShowCaseList = params => { return instance.post(`system/showCase/getShowCaseList`, params ); };
+//获取橱窗详情
+export const getShowCase = params => { return instance.post(`system/showCase/getShowCase`, params ); };
 //获取导航列表
 export const getNavigationList = params => { return instance.post(`system/navigation/getNavigationList`, params ); };
 
